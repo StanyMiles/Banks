fe457f5f3433b7584c48264adab0aadeb2fa38ee
Stany Miles <stanymiles@gmail.com>
2020-10-18 07:38:11 +0300

    Initial Commit

---
+++ Banks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banks
+//
+//  Created by Stanislav Kobiletski on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
